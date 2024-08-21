2d324374cf3d0bf819d1d11eafa6908796d92424
Luis Hernandez <luishernandz2503@gmail.com>
2024-08-21 00:59:22 -0400

    mock data

---
+++ lib/utils/mock/mockcomicdetailjson.dart
@@ -0,0 +1,126 @@
+class ComicDetailjsonMock {
+  final jsonImagen = {
+    "error": "OK",
+    "limit": 1,
+    "offset": 0,
+    "number_of_page_results": 1,
+    "number_of_total_results": 1,
+    "status_code": 1,
+    "results": {
+      "id": 1067394,
+      "image": {
+        "icon_url":
+            "https://comicvine.gamespot.com/a/uploads/square_avatar/11154/111547112/9450117-9414257100-81Zcx.jpg",
+        "medium_url":
+            "https://comicvine.gamespot.com/a/uploads/scale_medium/11154/111547112/9450117-9414257100-81Zcx.jpg",
+        "screen_url":
+            "https://comicvine.gamespot.com/a/uploads/screen_medium/11154/111547112/9450117-9414257100-81Zcx.jpg",
+        "screen_large_url":
+            "https://comicvine.gamespot.com/a/uploads/original/11154/111547112/9450117-9414257100-81Zcx.jpg",
+        "small_url":
+            "https://comicvine.gamespot.com/a/uploads/scale_small/11154/111547112/9450117-9414257100-81Zcx.jpg",
+        "super_url":
+            "https://comicvine.gamespot.com/a/uploads/scale_large/11154/111547112/9450117-9414257100-81Zcx.jpg",
+        "thumb_url":
+            "https://comicvine.gamespot.com/a/uploads/scale_super/11154/111547112/9450117-9414257100-81Zcx.jpg",
+        "tiny_url":
+            "https://comicvine.gamespot.com/a/uploads/square_mini/11154/111547112/9450117-9414257100-81Zcx.jpg",
+        "original_url":
+            "https://comicvine.gamespot.com/a/uploads/original/11154/111547112/9450117-9414257100-81Zcx.jpg",
+        "image_tags": "All Images"
+      },
+    },
+    "version": "1.0"
+  };
+  final jsonComicDetail = {
+    "error": "OK",
+    "limit": 1,
+    "offset": 0,
+    "number_of_page_results": 1,
+    "number_of_total_results": 1,
+    "status_code": 1,
+    "results": {
+      "aliases": null,
+      "api_detail_url":
+          "https://comicvine.gamespot.com/api/issue/4000-1067394/",
+      "associated_images": [],
+      "character_credits": [],
+      "character_died_in": [],
+      "concept_credits": [
+        {
+          "api_detail_url":
+              "https://comicvine.gamespot.com/api/concept/4015-56579/",
+          "id": 56579,
+          "name": "Translated Comic",
+          "site_detail_url":
+              "https://comicvine.gamespot.com/translated-comic/4015-56579/"
+        }
+      ],
+      "cover_date": "2024-08-20",
+      "date_added": "2024-08-20 15:22:11",
+      "date_last_updated": "2024-08-20 15:22:45",
+      "deck": null,
+      "description":
+          "<p><em>Taiyo somehow manages to survive the Silver-Rank spy exam, earning his promotion. He finally gets to talk to Kawashita and relay Tsubomi’s desire to perish. He quickly learns that Kawashita also faces an unenviable fate. To make matters worse, Momo Yozakura appears!</em></p>",
+      "first_appearance_characters": null,
+      "first_appearance_concepts": null,
+      "first_appearance_locations": null,
+      "first_appearance_objects": null,
+      "first_appearance_storyarcs": null,
+      "first_appearance_teams": null,
+      "has_staff_review": false,
+      "id": 1067394,
+      "image": {
+        "icon_url":
+            "https://comicvine.gamespot.com/a/uploads/square_avatar/11154/111547112/9450117-9414257100-81Zcx.jpg",
+        "medium_url":
+            "https://comicvine.gamespot.com/a/uploads/scale_medium/11154/111547112/9450117-9414257100-81Zcx.jpg",
+        "screen_url":
+            "https://comicvine.gamespot.com/a/uploads/screen_medium/11154/111547112/9450117-9414257100-81Zcx.jpg",
+        "screen_large_url":
+            "https://comicvine.gamespot.com/a/uploads/original/11154/111547112/9450117-9414257100-81Zcx.jpg",
+        "small_url":
+            "https://comicvine.gamespot.com/a/uploads/scale_small/11154/111547112/9450117-9414257100-81Zcx.jpg",
+        "super_url":
+            "https://comicvine.gamespot.com/a/uploads/scale_large/11154/111547112/9450117-9414257100-81Zcx.jpg",
+        "thumb_url":
+            "https://comicvine.gamespot.com/a/uploads/scale_super/11154/111547112/9450117-9414257100-81Zcx.jpg",
+        "tiny_url":
+            "https://comicvine.gamespot.com/a/uploads/square_mini/11154/111547112/9450117-9414257100-81Zcx.jpg",
+        "original_url":
+            "https://comicvine.gamespot.com/a/uploads/original/11154/111547112/9450117-9414257100-81Zcx.jpg",
+        "image_tags": "All Images"
+      },
+      "issue_number": "12",
+      "location_credits": [],
+      "name": "New Cherry Blossoms",
+      "object_credits": [],
+      "person_credits": [
+        {
+          "api_detail_url":
+              "https://comicvine.gamespot.com/api/person/4040-86507/",
+          "id": 86507,
+          "name": "Hitsuji Gondaira",
+          "site_detail_url":
+              "https://comicvine.gamespot.com/hitsuji-gondaira/4040-86507/",
+          "role": "writer, artist, cover"
+        }
+      ],
+      "site_detail_url":
+          "https://comicvine.gamespot.com/mission-yozakura-family-12-new-cherry-blossoms/4000-1067394/",
+      "store_date": "2024-08-20",
+      "story_arc_credits": [],
+      "team_credits": [],
+      "team_disbanded_in": [],
+      "volume": {
+        "api_detail_url":
+            "https://comicvine.gamespot.com/api/volume/4050-132678/",
+        "id": 132678,
+        "name": "Mission: Yozakura Family",
+        "site_detail_url":
+            "https://comicvine.gamespot.com/mission-yozakura-family/4050-132678/"
+      }
+    },
+    "version": "1.0"
+  };
+}
---
+++ lib/utils/mock/mockcomicjson.dart
@@ -0,0 +1,248 @@
+class Mockcomicjson {
+  final json = {
+    "error": "OK",
+    "limit": 100,
+    "offset": 0,
+    "number_of_page_results": 100,
+    "number_of_total_results": 1014403,
+    "status_code": 1,
+    "results": [
+      {
+        "aliases": null,
+        "api_detail_url":
+            "https://comicvine.gamespot.com/api/issue/4000-117874/",
+        "cover_date": "1952-07-01",
+        "date_added": "2008-06-06 11:09:04",
+        "date_last_updated": "2022-07-01 06:59:06",
+        "deck": null,
+        "description": null,
+        "has_staff_review": false,
+        "id": 117874,
+        "image": {
+          "icon_url":
+              "https://comicvine.gamespot.com/a/uploads/square_avatar/0/9116/893409-161.jpg",
+          "medium_url":
+              "https://comicvine.gamespot.com/a/uploads/scale_medium/0/9116/893409-161.jpg",
+          "screen_url":
+              "https://comicvine.gamespot.com/a/uploads/screen_medium/0/9116/893409-161.jpg",
+          "screen_large_url":
+              "https://comicvine.gamespot.com/a/uploads/screen_kubrick/0/9116/893409-161.jpg",
+          "small_url":
+              "https://comicvine.gamespot.com/a/uploads/scale_small/0/9116/893409-161.jpg",
+          "super_url":
+              "https://comicvine.gamespot.com/a/uploads/scale_large/0/9116/893409-161.jpg",
+          "thumb_url":
+              "https://comicvine.gamespot.com/a/uploads/scale_avatar/0/9116/893409-161.jpg",
+          "tiny_url":
+              "https://comicvine.gamespot.com/a/uploads/square_mini/0/9116/893409-161.jpg",
+          "original_url":
+              "https://comicvine.gamespot.com/a/uploads/original/0/9116/893409-161.jpg",
+          "image_tags": "All Images"
+        },
+        "associated_images": [],
+        "issue_number": "161",
+        "name": null,
+        "site_detail_url":
+            "https://comicvine.gamespot.com/jumbo-comics-161/4000-117874/",
+        "store_date": null,
+        "volume": {
+          "api_detail_url":
+              "https://comicvine.gamespot.com/api/volume/4050-769/",
+          "id": 769,
+          "name": "Jumbo Comics",
+          "site_detail_url":
+              "https://comicvine.gamespot.com/jumbo-comics/4050-769/"
+        }
+      },
+      {
+        "aliases": null,
+        "api_detail_url":
+            "https://comicvine.gamespot.com/api/issue/4000-92466/",
+        "cover_date": "1940-01-01",
+        "date_added": "2008-06-06 11:09:04",
+        "date_last_updated": "2022-07-01 06:29:27",
+        "deck": null,
+        "description": "<br />",
+        "has_staff_review": false,
+        "id": 92466,
+        "image": {
+          "icon_url":
+              "https://comicvine.gamespot.com/a/uploads/square_avatar/0/4/59471-766-92466-1-single-series.jpg",
+          "medium_url":
+              "https://comicvine.gamespot.com/a/uploads/scale_medium/0/4/59471-766-92466-1-single-series.jpg",
+          "screen_url":
+              "https://comicvine.gamespot.com/a/uploads/screen_medium/0/4/59471-766-92466-1-single-series.jpg",
+          "screen_large_url":
+              "https://comicvine.gamespot.com/a/uploads/screen_kubrick/0/4/59471-766-92466-1-single-series.jpg",
+          "small_url":
+              "https://comicvine.gamespot.com/a/uploads/scale_small/0/4/59471-766-92466-1-single-series.jpg",
+          "super_url":
+              "https://comicvine.gamespot.com/a/uploads/scale_large/0/4/59471-766-92466-1-single-series.jpg",
+          "thumb_url":
+              "https://comicvine.gamespot.com/a/uploads/scale_avatar/0/4/59471-766-92466-1-single-series.jpg",
+          "tiny_url":
+              "https://comicvine.gamespot.com/a/uploads/square_mini/0/4/59471-766-92466-1-single-series.jpg",
+          "original_url":
+              "https://comicvine.gamespot.com/a/uploads/original/0/4/59471-766-92466-1-single-series.jpg",
+          "image_tags": "All Images"
+        },
+        "associated_images": [],
+        "issue_number": "23",
+        "name": "Tailspin Tommy",
+        "site_detail_url":
+            "https://comicvine.gamespot.com/single-series-23-tailspin-tommy/4000-92466/",
+        "store_date": null,
+        "volume": {
+          "api_detail_url":
+              "https://comicvine.gamespot.com/api/volume/4050-766/",
+          "id": 766,
+          "name": "Single Series",
+          "site_detail_url":
+              "https://comicvine.gamespot.com/single-series/4050-766/"
+        }
+      },
+      {
+        "aliases": null,
+        "api_detail_url":
+            "https://comicvine.gamespot.com/api/issue/4000-117859/",
+        "cover_date": "1943-10-01",
+        "date_added": "2008-06-06 11:09:04",
+        "date_last_updated": "2022-07-01 06:44:04",
+        "deck": null,
+        "description": null,
+        "has_staff_review": false,
+        "id": 117859,
+        "image": {
+          "icon_url":
+              "https://comicvine.gamespot.com/a/uploads/square_avatar/0/9116/888590-56.jpg",
+          "medium_url":
+              "https://comicvine.gamespot.com/a/uploads/scale_medium/0/9116/888590-56.jpg",
+          "screen_url":
+              "https://comicvine.gamespot.com/a/uploads/screen_medium/0/9116/888590-56.jpg",
+          "screen_large_url":
+              "https://comicvine.gamespot.com/a/uploads/screen_kubrick/0/9116/888590-56.jpg",
+          "small_url":
+              "https://comicvine.gamespot.com/a/uploads/scale_small/0/9116/888590-56.jpg",
+          "super_url":
+              "https://comicvine.gamespot.com/a/uploads/scale_large/0/9116/888590-56.jpg",
+          "thumb_url":
+              "https://comicvine.gamespot.com/a/uploads/scale_avatar/0/9116/888590-56.jpg",
+          "tiny_url":
+              "https://comicvine.gamespot.com/a/uploads/square_mini/0/9116/888590-56.jpg",
+          "original_url":
+              "https://comicvine.gamespot.com/a/uploads/original/0/9116/888590-56.jpg",
+          "image_tags": "All Images"
+        },
+        "associated_images": [],
+        "issue_number": "56",
+        "name": null,
+        "site_detail_url":
+            "https://comicvine.gamespot.com/jumbo-comics-56/4000-117859/",
+        "store_date": null,
+        "volume": {
+          "api_detail_url":
+              "https://comicvine.gamespot.com/api/volume/4050-769/",
+          "id": 769,
+          "name": "Jumbo Comics",
+          "site_detail_url":
+              "https://comicvine.gamespot.com/jumbo-comics/4050-769/"
+        }
+      },
+      {
+        "aliases": null,
+        "api_detail_url":
+            "https://comicvine.gamespot.com/api/issue/4000-117857/",
+        "cover_date": "1942-07-01",
+        "date_added": "2008-06-06 11:09:04",
+        "date_last_updated": "2022-07-01 06:41:06",
+        "deck": null,
+        "description": null,
+        "has_staff_review": false,
+        "id": 117857,
+        "image": {
+          "icon_url":
+              "https://comicvine.gamespot.com/a/uploads/square_avatar/0/8015/231198-769-117857-1-jumbo-comics.jpg",
+          "medium_url":
+              "https://comicvine.gamespot.com/a/uploads/scale_medium/0/8015/231198-769-117857-1-jumbo-comics.jpg",
+          "screen_url":
+              "https://comicvine.gamespot.com/a/uploads/screen_medium/0/8015/231198-769-117857-1-jumbo-comics.jpg",
+          "screen_large_url":
+              "https://comicvine.gamespot.com/a/uploads/screen_kubrick/0/8015/231198-769-117857-1-jumbo-comics.jpg",
+          "small_url":
+              "https://comicvine.gamespot.com/a/uploads/scale_small/0/8015/231198-769-117857-1-jumbo-comics.jpg",
+          "super_url":
+              "https://comicvine.gamespot.com/a/uploads/scale_large/0/8015/231198-769-117857-1-jumbo-comics.jpg",
+          "thumb_url":
+              "https://comicvine.gamespot.com/a/uploads/scale_avatar/0/8015/231198-769-117857-1-jumbo-comics.jpg",
+          "tiny_url":
+              "https://comicvine.gamespot.com/a/uploads/square_mini/0/8015/231198-769-117857-1-jumbo-comics.jpg",
+          "original_url":
+              "https://comicvine.gamespot.com/a/uploads/original/0/8015/231198-769-117857-1-jumbo-comics.jpg",
+          "image_tags": "All Images"
+        },
+        "associated_images": [],
+        "issue_number": "41",
+        "name": null,
+        "site_detail_url":
+            "https://comicvine.gamespot.com/jumbo-comics-41/4000-117857/",
+        "store_date": null,
+        "volume": {
+          "api_detail_url":
+              "https://comicvine.gamespot.com/api/volume/4050-769/",
+          "id": 769,
+          "name": "Jumbo Comics",
+          "site_detail_url":
+              "https://comicvine.gamespot.com/jumbo-comics/4050-769/"
+        }
+      },
+      {
+        "aliases": null,
+        "api_detail_url":
+            "https://comicvine.gamespot.com/api/issue/4000-92458/",
+        "cover_date": "1939-01-01",
+        "date_added": "2008-06-06 11:09:04",
+        "date_last_updated": "2022-07-01 06:25:57",
+        "deck": null,
+        "description": "<br />",
+        "has_staff_review": false,
+        "id": 92458,
+        "image": {
+          "icon_url":
+              "https://comicvine.gamespot.com/a/uploads/square_avatar/0/4/59463-766-92458-1-single-series.jpg",
+          "medium_url":
+              "https://comicvine.gamespot.com/a/uploads/scale_medium/0/4/59463-766-92458-1-single-series.jpg",
+          "screen_url":
+              "https://comicvine.gamespot.com/a/uploads/screen_medium/0/4/59463-766-92458-1-single-series.jpg",
+          "screen_large_url":
+              "https://comicvine.gamespot.com/a/uploads/screen_kubrick/0/4/59463-766-92458-1-single-series.jpg",
+          "small_url":
+              "https://comicvine.gamespot.com/a/uploads/scale_small/0/4/59463-766-92458-1-single-series.jpg",
+          "super_url":
+              "https://comicvine.gamespot.com/a/uploads/scale_large/0/4/59463-766-92458-1-single-series.jpg",
+          "thumb_url":
+              "https://comicvine.gamespot.com/a/uploads/scale_avatar/0/4/59463-766-92458-1-single-series.jpg",
+          "tiny_url":
+              "https://comicvine.gamespot.com/a/uploads/square_mini/0/4/59463-766-92458-1-single-series.jpg",
+          "original_url":
+              "https://comicvine.gamespot.com/a/uploads/original/0/4/59463-766-92458-1-single-series.jpg",
+          "image_tags": "All Images"
+        },
+        "associated_images": [],
+        "issue_number": "2",
+        "name": "Broncho Bill",
+        "site_detail_url":
+            "https://comicvine.gamespot.com/single-series-2-broncho-bill/4000-92458/",
+        "store_date": null,
+        "volume": {
+          "api_detail_url":
+              "https://comicvine.gamespot.com/api/volume/4050-766/",
+          "id": 766,
+          "name": "Single Series",
+          "site_detail_url":
+              "https://comicvine.gamespot.com/single-series/4050-766/"
+        }
+      },
+    ],
+    "version": "1.0"
+  };
+}
